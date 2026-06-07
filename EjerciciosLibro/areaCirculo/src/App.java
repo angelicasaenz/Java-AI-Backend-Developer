576844d7d466c903433bf2c7b596ab969d52810e
Angelica <msaenzc06@gmail.com>
2026-06-07 17:59:03 -0500

    Ejercicio areaCirculo

--- EjerciciosLibro/areaCirculo/src/App.java
+++ EjerciciosLibro/areaCirculo/src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class App {
+
+    // 2. Pedir el radio de un círculo y calcular su área. A=PI*r^2.
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el radio de un circulo: ");
+        double radio = sc.nextDouble();
+        double area = (Math.PI * Math.pow(radio, 2));
+        System.out.println("El área del circulo es: " + area);
+        sc.close();
     }
 }
