a4707d83db1bad53339aaa43a0e2a69d373cb24e
Angélica Sáenz <114958387+angelicasaenz@users.noreply.github.com>
2026-07-06 01:29:42 -0500

    Se agrega metodo para verificar si hay producto

--- Clase4/gestorInventario/src/Operaciones.java
+++ Clase4/gestorInventario/src/Operaciones.java
@@ -7,20 +7,30 @@ public class Operaciones {
     static double precio = 0.0;
     static boolean hayProducto = false;
 
+    //Validar que no hay producto
+
+    public static boolean noHayProducto(){
+        if (!hayProducto){
+            System.out.println("Aún no hay producto registrado");
+            return true;
+        }
+        return false;
+    }
+
     public static void registrar(Scanner sc){
 
         nombre = Validador.leerTexto(sc, "Nombre del producto: ");
         cantidad = Validador.leerEntero(sc, "Cantidad: ");
         precio = Validador.leerDouble(sc, "Precio: ");
         hayProducto = true;
-        System.out.println("");
+        System.out.println();
         System.out.println("Producto registrado exitosamente.");
     }
 
     public static void verProducto(Scanner sc){
-        if (!hayProducto){
-            System.out.println("Aún no hay producto registrado");
-        } else {
+        if (noHayProducto()){
+            return;
+        }else {
             System.out.println("Nombre: " + nombre);
             System.out.println("Cantidad: " + cantidad);
             System.out.println("Precio: " + precio);
@@ -28,8 +38,8 @@ public class Operaciones {
     }
 
     public static void actualizarCantidad(Scanner sc){
-        if (!hayProducto){
-            System.out.println("Aún no hay producto registrado");
+        if (noHayProducto()){
+            return;
         } else {
             cantidad = Validador.leerEntero(sc, "Cantidad nueva: ");
             System.out.println("Se ha actualizado correctamente");
@@ -37,8 +47,8 @@ public class Operaciones {
     }
 
     public static void verValorTotal(Scanner sc){
-        if (!hayProducto){
-            System.out.println("Aún no hay producto registrado");
+        if (noHayProducto()){
+            return;
         } else {
             double valorTotal = cantidad * precio;
             System.out.println("El total es: " + valorTotal);
--- Clase4/gestorInventario/src/Validador.java
+++ Clase4/gestorInventario/src/Validador.java
@@ -40,4 +40,6 @@ public class Validador {
         return sc.nextLine();
     }
 
+
+
 }
